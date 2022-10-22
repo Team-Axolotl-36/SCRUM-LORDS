2db37a8d52fe3488db42168339b6f5a7076ceb1c
chrishicks430 <chrishicks430@gmail.com>
2022-10-22 13:44:23 -0400

    working db and boilerplate backend chris/pierce

---
+++ server/controllers/scrumController.js
@@ -0,0 +1,35 @@
+const models = require('../models/scrumModel');
+
+const scrumController = {};
+
+scrumController.getCharacters = (req, res, next) => {
+  // write code here
+
+  next();
+};
+
+scrumController.getSpecies = (req, res, next) => {
+  // write code here
+
+  next();
+};
+
+scrumController.getHomeworld = (req, res, next) => {
+  // write code here
+
+  next();
+};
+
+scrumController.getFilm = (req, res, next) => {
+  // write code here
+
+  next();
+};
+
+scrumController.addCharacter = (req, res, next) => {
+  // write code here
+
+  next();
+};
+
+module.exports = scrumController;
