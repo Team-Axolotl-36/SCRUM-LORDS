3f7a387180779b7ddbfdf416df411132ea8b40ec
chrishicks430 <chrishicks430@gmail.com>
2022-10-21 20:03:41 -0400

    webpack and setup complete

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+//const testing = require('../routes/test');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', express.static(path.resolve(__dirname, '../build')));
+
+
+
+
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' }, 
+  };
+  return res.status(defaultErr.status).send();
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+  
+module.exports = app;
