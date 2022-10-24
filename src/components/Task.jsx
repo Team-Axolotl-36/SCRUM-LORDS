f7edbb219d8c574f2e9da12b0106c0bdc79d3e98
Ethan Liu <eliu080893@gmail.com>
2022-10-24 11:56:30 -0400

    created the PATCH request to update a task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -5,6 +5,32 @@ const Task = (props) => {
   const [priority, setPriority] = useState('high');
 
 
+  function handleUpdate(){
+    const refineDate = new Date();
+    const updateObj = {
+      project: props.projectTitle,
+      projectId: props.projectId,
+      task: value,
+      taskId: props.taskId,
+      priority: priority,
+      currentDate: refineDate.toDateString()
+    };
+
+    fetch('/',{
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify(updateObj)
+    })
+      .then(resp => resp.json())
+      .then((data) => {
+        console.log('this is data from fetch:', data);
+      })
+      .catch((err) => {
+        console.log('Error :', err);
+      });
+  }
+
+
   function handleSave(){
     const refineDate = new Date();
     const finalObj = {
@@ -41,6 +67,7 @@ const Task = (props) => {
 
       <button onClick={handleSave}> + </button>
       <button onClick={(e) => props.delete(e, props.taskId, props.projectId, props.projectTitle, value)}> X </button>
+      <button onClick={handleUpdate}> update/save task.</button>
       
     </div>
   );
