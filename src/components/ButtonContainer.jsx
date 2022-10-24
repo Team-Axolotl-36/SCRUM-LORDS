45cadd4dfb0497747de1b4eb271029b283dd0108
Ethan Liu <eliu080893@gmail.com>
2022-10-24 10:43:45 -0400

    update 2

--- src/components/ButtonContainer.jsx
+++ src/components/ButtonContainer.jsx
@@ -4,35 +4,37 @@ import Project from './Project.jsx';
 const ButtonContainer = (props) => {
   const [input, setInput] = useState('');
   const [projects, setProjects] = useState([]);
+  const [projectId, setProjectId] = useState(0);
 
   function handleSubmit(e) {
     e.preventDefault();
-    // console.log('this is input:', e.target.value);
+    
+    setProjectId( (state) => state + 1);
 
-    const newProject = ( <Project projectTitle={input} key={input}/> );
+    const newProject = ( <Project projectTitle={input} key={input} id={projectId}/> );
     
     setProjects( [...projects, newProject] );
-  
-    // fetch('/',{
-    //   method: 'POST',
-    //   headers: { 'Content-Type': 'application/json'},
-    //   body: JSON.stringify({hello: 'world'})
-    // })
-    //   .then(resp => resp.json())
-    //   .then((data) => {
-    //     console.log('this is data from fetch:', data);
-    //   })
-    //   .catch((err) => {
-    //     console.log('Error :', err);
-    //   });
   }
+
+  // fetch('/',{
+  //   method: 'POST',
+  //   headers: { 'Content-Type': 'application/json'},
+  //   body: JSON.stringify({hello: 'world'})
+  // })
+  //   .then(resp => resp.json())
+  //   .then((data) => {
+  //     console.log('this is data from fetch:', data);
+  //   })
+  //   .catch((err) => {
+  //     console.log('Error :', err);
+  //   });
 
 
   return (
     <div id='buttonContainer'>
       <button 
         onClick={handleSubmit}>
-        Button to add task.
+          Button to add task.
       </button>
       <input placeholder='Title of the project' required onChange={((e) => {setInput(e.target.value);}) } type="text" />
       {projects}
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,25 +3,25 @@ import Task from './Task.jsx';
 
 const Project = (props) => {
   const [task, setTask] = useState([]);
+  const [taskId, setTaskId] = useState(0);
 
   function handleClick() {
 
-    const newTask = (<Task projectTitle={props.projectTitle} key={new Date()} delete={deleteTask} id={Math.floor(Math.random() * 10000)} />);
+    setTaskId( (state) => taskId + 1);
+
+    const newTask = (<Task projectTitle={props.projectTitle} key={taskId} delete={deleteTask} id={taskId}/>);
     setTask([...task, newTask]);
     console.log(task.length);
   }
 
-  function deleteTask(key, id) {
-    // console.log('event: ', id);
-    // console.log('tasks :', task);
-    
-    setTask((prevTasks) => {
+  function deleteTask(e, bubble) {
+
+    setTask( (prevTasks) => {
       return prevTasks.filter((el) => {
-        console.log('el :', el);
-        return el.props.id !== id;
+        return el.props.id !== bubble;
       });
     });
-    
+
     // we might need to send ID (to the backend) when deleting in order to identify which task needs to be deleted 
 
     // fetch('/delete/task', {
@@ -36,17 +36,15 @@ const Project = (props) => {
     //   .catch((err) => {
     //     console.log('Error : ', err);
     //   });
-    
   }
   
   return (
     <div className="project">
-        Project : {props.projectTitle}
+      <p>Project ID: {props.id} </p>
+      <p>Project Title: {props.projectTitle} </p>
       <button onClick={handleClick}> Create a new task.</button>
       
       {task}
     </div>
   );
 };
-
-export default Project;
